8c3d9c6adc73bfdacaffab41e4cc8210c9283f0c
qifeng <2713247954@qq.com>
2022-06-09 00:17:22 +0800

    Promise对象语法

--- 03_Promise/02-Promise对象语法.js
+++ 03_Promise/02-Promise对象语法.js
@@ -1,8 +1,6 @@
 // 1.Promise 用于解决异步函数嵌套问题，本身是一个构造函数，所以需要new创建;
 // 2.Promise 构造函数的形参是一个函数，这个函数中一定要放入异步函数;
 // 3.Promise 形参的函数，有两个形参，一个处理成功的结果，一个处理失败的结果;
-// 成功就交由resolve()处理
-// 失败就交由reject()处理
 
 import fs from "fs";
 
@@ -14,9 +12,9 @@ let p1 = new Promise((resolve, reject) => {
         //     resolve(data);
         // }
         if (err == null) {
-            resolve(data);
+            resolve(data); // 成功就交由resolve()处理
         } else {
-            reject(err);
+            reject(err); // 失败就交由reject()处理
         }
     });
 });
