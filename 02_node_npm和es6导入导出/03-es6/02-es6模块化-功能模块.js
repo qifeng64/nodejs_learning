86878419927410d5597c57857f21e6a97d214cea
qifeng <2713247954@qq.com>
2022-06-09 20:22:01 +0800

    ES6模块化导入导出

--- 02_node_npm和es6导入导出/03-es6/02-es6模块化-功能模块.js
+++ 02_node_npm和es6导入导出/03-es6/02-es6模块化-功能模块.js
@@ -1,7 +1,7 @@
-// 1.默认导入导出 - 不能进行解构
+// 1.默认导出 - 不能进行解构
 export default { name: "张三", age: 19 };
 
-// 2.按需导入与按需导出 - 接收必须解构
+// 2.按需导出 - 接收必须解构
 export var num = 123;
 export let str = 456;
 export const fn = function () {
